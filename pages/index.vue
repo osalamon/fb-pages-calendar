7f37f75c1c6a2cc7b11dd75083825e6e33d1bd9c
Ondrej Salamon <ondrej.salamon@digitalbox.cz>
2019-05-14 14:53:47 +0200

    nuxt default + some playing w/ components&pages

---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <section>
+    <div class="container">
+      <h1 class="title">
+        fb-pages-calendar
+      </h1>
+      <h2 class="subtitle">
+        A simple app for displaying events from FB pages you don&#39;t want to
+        miss
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Navbar from '~/components/Navbar.vue'
+
+export default {
+  components: {
+    Logo,
+    Navbar
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
